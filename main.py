a9f6a3d1da489ff5e29035e0a18b74aac4ed0e40
dougmc0 <dougmc@frenzied.us>
2025-01-06 11:13:21 -0600

    .

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
-def sort_on(dict):
-    return dict["num"]
-
-
 def get_text():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         return file_contents
 
-file_contents = get_text()
+def print_word_count(text):
+    words = text.split()
+    print(f"Number of words in file: {len(words)}")
 
-words = file_contents.split()
-print(f"Number of words in file: {len(words)}")
+def print_letter_counts(text):
+    chars = {}
+    for word in text:
+        for char in word:
+            char = char.lower()
+            if char in chars:
+                chars[char] += 1
+            else:
+                chars[char] = 1
 
-chars = {}
-for word in words:
-    for char in word:
-        char = char.lower()
-        if char in chars:
-            chars[char] += 1
-        else:
-            chars[char] = 1
+    for char in sorted(chars, key=chars.get, reverse=True):
+        print(f"Number of '{char}' in file: {chars[char]}")
 
-for char in sorted(chars, key=chars.get, reverse=True):
-    print(f"Number of '{char}' in file: {chars[char]}") 
 
+if __name__ == "__main__":
+    text = get_text()
+    print_word_count(text)
+    print_letter_counts(text)
